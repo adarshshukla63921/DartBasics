1ab7a4e34e95169710ff132c7199df378d2e4f92
adarshshukla63921 <adarshshukla63921@gmail.com>
2025-12-11 19:34:21 +0530

    Datatypes and HelloWorld

---
+++ Datatypes/boolDatatypes.dart
@@ -0,0 +1,5 @@
+void main(){
+  bool isDartFun = true; // only true or false values.
+  print("Is Dart fun ? : ${isDartFun}");
+  print("Is chocolate bitter ? : ${!isDartFun}");
+}
---
+++ Datatypes/numericDatatypes.dart
@@ -0,0 +1,10 @@
+void main() {
+  // int, double and num. 
+  int firstInt = 10;
+  double firstDouble = 10.5;
+  num firstNum = 3.1456;
+
+  print("First integer : ${firstInt}");
+  print("First double : ${firstDouble}");
+  print("First num : ${firstNum} where num can hold both int and double values.");
+}

956de66735fd93e9bacadb2cf9e63259d621ba45
adarshshukla63921 <adarshshukla63921@gmail.com>
2025-12-13 00:01:44 +0530

    Functions

---
+++ Functions/basics.dart
@@ -0,0 +1,13 @@
+// returnType name(argumentType argumentName){
+      // function body
+// }
+int add(int a, int b) {
+  return a + b;
+}
+
+void main(){
+
+  // a function is simply a block of reusable code that performs a specific task
+  int sum = add(3, 5);
+  print("The sum is: $sum");
+}
